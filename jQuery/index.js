93e3088adce0a78a609673952b0f4044739d66c4
Himanshu Kumar <himanshukumar02122@gmail.com>
2022-07-14 16:04:57 +0530

    update

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,4 +2,16 @@ $(()=>{
     $("p").on('click',()=>{
       $("p").hide();
     });
+
+    $('#footer-plus-icon').on('click',()=> {
+      $('#footer-plus-icon').hide();
+      $('#footer-minus-icon').show();
+      $('#ul-toggle').show();
+    })
+
+    $('#footer-minus-icon').on('click',()=> {
+      $('#footer-minus-icon').hide();
+      $('#footer-plus-icon').show();
+      $('#ul-toggle').hide();
+    })
   });
